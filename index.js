be52ad2e07cacfb97b590d8fa3f5058f2f8facfe
FernandoAndrada <fernandojcandrada@gmail.com>
2022-11-08 14:42:19 -0300

    Cria [M2S07]-String e Number

---
+++ index.js
@@ -0,0 +1,6 @@
+let number= 14.29;
+
+   let texto =  (`Há alguns minutos atrás eram ${(number)}hs` 
+    + ` e daqui a alguns minutos será ${((number)+0.10).toFixed(2)}hs.`);
+    
+console.log(texto);
